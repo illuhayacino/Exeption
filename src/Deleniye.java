a54a3f350a80513cdb955c6dc71a702b5c9c6687
illuhayacino <56147785+illuhayacino@users.noreply.github.com>
2020-12-24 11:58:35 +0300

    without catch, with finally

--- src/Deleniye.java
+++ src/Deleniye.java
@@ -1,2 +1,28 @@
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
 public class Deleniye {
+
+    public static int getAreaValue(int x, int y){
+        if(y == 0) throw new ArithmeticException("value of 'y' is y="+y);
+        return x/y;
+    }
+
+    public static void main(String[] args) {
+
+        int result = 0;
+
+        try {
+            result = getAreaValue(12, 0);
+
+        }
+        finally {
+            System.out.println("result:" + result);
+        }
+
+
+
+    }
+
 }
